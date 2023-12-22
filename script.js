9f20ab9f550d728d6e7937317b0fd209ca98afd3
pradeepkumar-es <createthefuture121@gmail.com>
2023-12-22 10:37:57 +0530

    Post 1 content and some styling is added

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-// comments Handling
-document.getElementById('commentForm').addEventListener('submit', function(event) {
+// comments Handling for Blog Post 1
+document.getElementById('commentForm1').addEventListener('submit', function(event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
-    const message = document.getElementById('message').value;
+    const name = document.getElementById('name1').value;
+    const message = document.getElementById('message1').value;
   
     const commentDiv = document.createElement('div');
     commentDiv.classList.add('comment');
     commentDiv.innerHTML = `<strong>${name}</strong>: ${message}`;
   
-    document.getElementById('commentsContainer').appendChild(commentDiv);
+    document.getElementById('commentsContainer1').appendChild(commentDiv);
   });
   
-// document.getElementById('commentForm2').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const name = document.getElementById('name2').value;
-//     const message = document.getElementById('message2').value;
+// Comment handling for blog post 2  
+document.getElementById('commentForm2').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name2').value;
+    const message = document.getElementById('message2').value;
   
-//     const commentDiv = document.createElement('div');
-//     commentDiv.classList.add('comment');
-//     commentDiv.innerHTML = `<strong>${name}</strong>: ${message}`;
+    const commentDiv = document.createElement('div');
+    commentDiv.classList.add('comment');
+    commentDiv.innerHTML = `<strong>${name}</strong>: ${message}`;
   
-//     document.getElementById('commentsContainer2').appendChild(commentDiv);
-//   });
+    document.getElementById('commentsContainer2').appendChild(commentDiv);
+  });
   
-// document.getElementById('commentForm3').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const name = document.getElementById('name3').value;
-//     const message = document.getElementById('message3').value;
+// comment handling for blog post 3
+document.getElementById('commentForm3').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name3').value;
+    const message = document.getElementById('message3').value;
   
-//     const commentDiv = document.createElement('div');
-//     commentDiv.classList.add('comment');
-//     commentDiv.innerHTML = `<strong>${name}</strong>: ${message}`;
+    const commentDiv = document.createElement('div');
+    commentDiv.classList.add('comment');
+    commentDiv.innerHTML = `<strong>${name}</strong>: ${message}`;
   
-//     document.getElementById('commentsContainer3').appendChild(commentDiv);
-//   });
+    document.getElementById('commentsContainer3').appendChild(commentDiv);
+  });
   
